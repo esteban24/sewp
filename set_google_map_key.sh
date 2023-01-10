dc97e72393b249254d42b977794b826b445d3ffd
esteban24 <esteban.martin.busto@gmail.com>
2023-01-10 12:26:44 +0100

    added set_google_map_key.sh script

---
+++ set_google_map_key.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# CHECK GOOGLE_MAPS_KEY ENV VARIABLE EXIST 
+[[ -z "${GOOGLE_MAPS_KEY}" ]] && GoogleMapsKey='default' || GoogleMapsKey="${GOOGLE_MAPS_KEY}"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "MacOS $OSTYPE detected..."
+    sed -i '' -e "s/__GOOGLE_MAPS_KEY__/${GoogleMapsKey}/g" index.html
+else
+    echo "$OSTYPE detected..."
+    sed -i -e "s/__GOOGLE_MAPS_KEY__/${GoogleMapsKey}/g" index.html
+fi
